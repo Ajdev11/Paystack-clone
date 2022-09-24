60515c4776f5ea0a1b0b7dade8ef882244c7e153
Ajdev11 <mosesogun11@gmail.com>
2022-09-24 05:00:22 -0700

    file: dropdown.jsx

--- src/PaystackComponent/Section1.jsx
+++ src/PaystackComponent/Section1.jsx
@@ -1,17 +1,17 @@
-import {React, useEffect } from 'react'
+import { React, useEffect } from "react";
 import { gsap } from "gsap";
-import { NavLink, Link } from "react-router-dom";
-import logo from "../asset/Paystack images/logo.png";
+import { NavLink } from "react-router-dom";
+import Dropdown from "./Dropdown";
 
 function Section1() {
-   
-      useEffect(() => {
-        const tl = gsap.timeline();
-        tl.from(".gsapElement", { opacity: 0, duration: 1, y: 100 });
-      });
+  useEffect(() => {
+    const tl = gsap.timeline();
+    tl.from(".gsapElement", { opacity: 0, duration: 1, y: 100 });
+  });
   return (
     <div>
-        <div className="section-1">
+      <div className="section-1">
+        <div>
           <h1 className="gsapElement">
             Modern Online and Offline <br />
             Payments for Africa
@@ -35,92 +35,12 @@ function Section1() {
                 Or Contact sales
               </NavLink>
             </div>
-            <div className="smallerIcons">
-              <Link className="links" to="/">
-                <img id="logoIcon" src={logo} alt="others" />
-                <div className="developer">
-                  <ul className="dropdown4">
-                    <ul className="grid-1">
-                      <li>
-                        <h4>
-                          Get paid from <span id="lagNig">Lagos,Nigeria</span>
-                        </h4>
-                        <span className="sell">
-                          Sell locally with four different <br />
-                          payment methods
-                        </span>
-                      </li>
-                    </ul>
-                  </ul>
-                </div>
-              </Link>
-            </div>
-          </div>
-          <div>
-            <Link className="links" to="/">
-              <img id="logoIcon2" src={logo} alt="others" />
-              <div className="developer">
-                <ul className="dropdown5">
-                  <ul className="grid-1">
-                    <li>
-                      <h4>
-                        Get paid from <span id="lagNig">Rio,Brazil</span>
-                      </h4>
-                      <span className="sell">
-                        Sell to customers all over the
-                        <br />
-                        world
-                      </span>
-                    </li>
-                  </ul>
-                </ul>
-              </div>
-            </Link>
-          </div>
-          <div className="smallerIcons2">
-            <Link className="links" to="/">
-              <img id="logoIcon3" src={logo} alt="others" />
-              <div className="developer">
-                <ul className="dropdown6">
-                  <ul className="grid-1">
-                    <li>
-                      <h4>
-                        Get paid from <span id="lagNig">Johannesburg</span>
-                      </h4>
-                      <span className="sell">
-                        Pyatscak grows with your
-                        <br />
-                        business
-                      </span>
-                    </li>
-                  </ul>
-                </ul>
-              </div>
-            </Link>
-          </div>
-          <div className="smallerIcons">
-            <Link className="links" to="/">
-              <img id="logoIcon4" src={logo} alt="others" />
-              <div className="developer">
-                <ul className="dropdown7">
-                  <ul className="grid-1">
-                    <li>
-                      <h4>
-                        Get paid from <span id="lagNig">Cairo,Egypt</span>
-                      </h4>
-                      <span className="sell">
-                        Accept payment in any currency <br />
-                        you want
-                      </span>
-                    </li>
-                  </ul>
-                </ul>
-              </div>
-            </Link>
           </div>
         </div>
+        <Dropdown />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Section1
+export default Section1;
